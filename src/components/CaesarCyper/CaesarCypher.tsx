e4afb100f6874e00114e83c9095de532a44f2216
dvhata <thaohanh233@gmail.com>
2021-09-17 03:34:19 +0700

    CaesarCypher encode

--- src/components/CaesarCyper/CaesarCypher.tsx
+++ src/components/CaesarCyper/CaesarCypher.tsx
@@ -3,13 +3,10 @@ import React from "react";
 export default function CaesarCypher() {
   const [textInput, setTextInput] = React.useState("");
   const [keyInput, setKeyInput] = React.useState(0);
-  //const [textAfterEncode,setTextAfterEncode] = React.useState("")
 
-  /* const handleSubmit = React.useCallback((e) => {
+  const handleChangeInputText = React.useCallback((e) => {
     setTextInput(e.target.value);
-    console.log(e.target.value);
   }, []);
- */
 
   const handleChangeInputKey = React.useCallback((e) => {
     let temp: number = parseInt(e.target.value);
@@ -17,10 +14,30 @@ export default function CaesarCypher() {
     setKeyInput(temp);
   }, []);
 
+  var output = "";
   function handleSetTextAfterEncode() {
     showNewCode();
-    const temp: string = textInput;
-    return temp;
+    // Go through each character
+    for (var i = 0; i < textInput.length; i++) {
+      // Get the character we'll be appending
+      var c = textInput[i];
+      // If it's a letter...
+      if (c.match(/[a-z]/i)) {
+        // Get its code
+        var code = textInput.charCodeAt(i);
+
+        // Uppercase letters
+        if (code >= 65 && code <= 90) {
+          c = String.fromCharCode(((code - 65 + keyInput) % 26) + 65);
+        }
+        // Lowercase letters
+        else if (code >= 97 && code <= 122) {
+          c = String.fromCharCode(((code - 97 + keyInput) % 26) + 97);
+        }
+      }
+      output += c;
+    }
+    return output;
   }
 
   const alphabet: string = "abcdefghijklmnopqrstuvwxyz";
@@ -29,26 +46,25 @@ export default function CaesarCypher() {
     code += alphabet.slice(keyInput, 26) + alphabet.slice(0, keyInput);
     return code;
   }
+
   const newInput = handleSetTextAfterEncode();
 
   return (
     <div>
+      <h2>CaesarCypher</h2>
       <h3>Your alphabet: {alphabet} </h3>
       <input
         type="text"
-        onChange={(e) => setTextInput(e.target.value)}
+        onChange={handleChangeInputText}
         placeholder="Enter your text"
       />
       <input onChange={handleChangeInputKey} placeholder="Enter your key" />
-      {/* <button value={textInput} onClick={handleSubmit}>
-        Encode
-      </button> */}
       <div>
         <h3>Your new code: {code}</h3>
       </div>
       <div>
         <h2>Your output here</h2>
-        <input value={keyInput}></input>
+        <input value={output}></input>
       </div>
     </div>
   );
